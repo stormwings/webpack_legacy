04646bac9fd6b0e6f6bf6fc23f97b1862c8c685a
Mariano V <marianov994@gmail.com>
2019-07-13 19:16:22 -0300

    babel async await functions plugin

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,28 @@
+import axios from 'axios'
 import '../css/index.css'
 
-const array = ['hello ', 'how ', 'are ', 'you']
-array.map(element => {
-  setTimeout(() => {
-    console.log(`${element}`)
-  }, 3000)
+const baseUrl = 'https://rickandmortyapi.com/api/'
+const characters = 'character/'
+const locations = 'location/'
+const episodes = 'episode/'
+
+const fetchApi = async (mode) => {
+  let result;
+  await axios.get(`${baseUrl}${mode}`)
+    .then(({
+      data
+    }) => {
+      result = data
+    })
+  return result
+}
+
+fetchApi(characters).then((result) => {
+  console.log(result)
+})
+fetchApi(locations).then((result) => {
+  console.log(result)
+})
+fetchApi(episodes).then((result) => {
+  console.log(result)
 })
