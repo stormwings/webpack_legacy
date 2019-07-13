8ebbbce543386dd4119d97d68a045cdc5e99812b
Mariano V <marianov994@gmail.com>
2019-07-13 05:42:22 -0300

    multy entry

---
+++ multiple/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+module.exports = {
+  // el archivo que va a iniciar a leer webpack
+  // usamos path para mejorar las ubicaciones
+  entry: {
+    home: path.resolve(__dirname, '../src/home.js'),
+    contact: path.resolve(__dirname, '../src/contact.js'),
+    blog: path.resolve(__dirname, '../src/blog.js')
+  },
+  // el modo de compresion y configuración
+  mode: 'development',
+  // donde irá a parar y como se llamará el archivo
+  output: {
+    // usamos path para manejar mejor las ubicaciones 
+    // dirname hace que se ubique en la raiz del proyecto
+    // los parametros siguientes seran las carpetas/archivos
+    path: path.resolve(__dirname, '../js'),
+    // nombre del archivo => [variable_de_entry].js
+    filename: '[name].js'
+  }
+}
