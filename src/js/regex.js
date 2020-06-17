7e6c984e2a80d20ea176ced7a3e471455a0bc942
Mariano V <marianov994@gmail.com>
2020-06-17 17:24:48 -0300

    regex principal rules

--- src/js/regex.js
+++ src/js/regex.js
@@ -1,3 +1,39 @@
+// Rules
+// \d => digits (equivalent [0-9])
+// \w => characters (equivalent [a-zA-Z0-9])
+// \s => empty spaces (equivalent [])
+
+// Delimiters
+// * => zero or many
+// + => one or many
+// ? => zero or one
+
+// Bracket
+// [clase] => all the 'clase'
+// [clase]{x,y} => between X and Y 'clase'
+// [clase]{x,} => at least one 'clase'
+
+// Specials
+// \. or \* or \\	=> escaped special characters
+// .+, => (without ?) match most biggest possible
+// .+?, => (with ?) match most smallest possible
+
+// Group
+// (abc) => capture group 'abc
+
+// Negation
+// [0-5a-c] = match regex
+// [^0-5a-c] = match all except regex
+
+// Start ^ and End $
+// ^\d{5,}$ = The match must be the entire line, or it is discarded (pending practice)
+
+// Examples
+// [cmf]an => can, man or fan
+// aa+b*c* => two a, next many c next any c
+// \d*\w*\w*\? =>	match "24 files found?"
+// I love (cats|dogs) => "i love cats" or "o love dogs"
+
 // validate by separated regex groups
 const regexData = /([0-9]{4})-([0-9]{2})-([0-9]{2})/;
 const match = regexData.exec('2020-02-14/');
